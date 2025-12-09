12f8da7923910eb9f09b5162355b46e5855252da
bytefairy0 <spacewalker1085@gmail.com>
2025-12-09 15:36:39 +0500

    changed 222

--- program2.py
+++ program2.py
@@ -2,8 +2,20 @@
 
 print("ok so this is much better pattern")
 s = '*'
-for i in range(5):
+for i in range(8):
 	print('*'*i)
 
 print()
+
+# ok changing this one too
+print("\n\nAnother pattern")
+for i in range(10):
+	for j in range(20):
+		print("*"*i, end=' ')
+	print()
+
+print()
+
+print("IDK waht i'm doing")
+
 
